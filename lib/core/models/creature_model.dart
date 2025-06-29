a555bb6522f59b913154202c066fb3fa0cb18c5d
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-06-29 16:58:37 -0300

    FEAT: Adding Attack List in Creature Model

--- lib/core/models/creature_model.dart
+++ lib/core/models/creature_model.dart
@@ -1,11 +1,13 @@
 import '../enums/elemento_enum.dart';
 import '../enums/raridade_enum.dart';
+import './attack_model.dart';
 
 class Creature {
   int vida;
   int ataque;
   List<Elemento> elementos;
+  List<Attack> ataques;
   Raridade raridade;
 
-  Creature(this.vida, this.ataque, this.elementos, this.raridade);
+  Creature(this.vida, this.ataque, this.elementos, this.raridade, this.ataques);
 }
