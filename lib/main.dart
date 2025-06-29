6f615fb5ec3ac7555edeb7ce7b267b5fb9bc2469
dallalaste <128847833+dallelaste0105@users.noreply.github.com>
2025-06-29 01:52:15 -0300

    feat: melhora de visualização

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-/* 1-fogo; 2-agua; 3-terra */
 
+/* 1-fogo; 2-agua; 3-terra */
 enum Elemento {
   fogo,
   agua,
@@ -14,6 +14,10 @@ class personagem {
   int ataque;
   Elemento elemento;
 
+  int atk1() {
+    return ataque * 50;
+  }
+
   personagem(this.vida, this.ataque, this.elemento);
 }
 
@@ -23,34 +27,30 @@ personagem kleber = personagem(2000, 350, Elemento.ar);
 personagem jogador = personagem(0, 0, Elemento.nulo);
 personagem inimigo = personagem(0, 0, Elemento.nulo);
 
-
-
-int luta(){
-    if (jogador.elemento == Elemento.agua && inimigo.elemento == Elemento.fogo || inimigo.elemento == Elemento.terra ){
-      inimigo.vida -= jogador.ataque * 3;
-    }
-    if (jogador.elemento == Elemento.terra && inimigo.elemento == Elemento.fogo || inimigo.elemento == Elemento.ar){
-      inimigo.vida -= jogador.ataque * 2;
-    }
-    if (jogador.elemento == Elemento.ar && inimigo.elemento == Elemento.fogo){
-      inimigo.vida -= jogador.ataque * 4;
-    }
-    else {inimigo.vida -= jogador.ataque;};
-    return 0;
+int luta() {
+  if ((jogador.elemento == Elemento.agua &&
+          (inimigo.elemento == Elemento.fogo || inimigo.elemento == Elemento.terra)) ||
+      (jogador.elemento == Elemento.terra &&
+          (inimigo.elemento == Elemento.fogo || inimigo.elemento == Elemento.ar)) ||
+      (jogador.elemento == Elemento.ar && inimigo.elemento == Elemento.fogo)) {
+    inimigo.vida -= jogador.ataque * 3;
+  } else {
+    inimigo.vida -= jogador.ataque;
+  }
+  return 0;
 }
 
-int luta_pelo_inimigo(){
-    if (inimigo.elemento == Elemento.agua && jogador.elemento == Elemento.fogo || jogador.elemento == Elemento.terra ){
-      jogador.vida -= inimigo.ataque * 3;
-    }
-    if (inimigo.elemento == Elemento.terra && jogador.elemento == Elemento.fogo || jogador.elemento == Elemento.ar){
-      jogador.vida -= inimigo.ataque * 2;
-    }
-    if (inimigo.elemento == Elemento.ar && jogador.elemento == Elemento.fogo){
-      jogador.vida -= inimigo.ataque * 4;
-    }
-    else {jogador.vida -= inimigo.ataque;};
-    return 0;
+int luta_pelo_inimigo() {
+  if ((inimigo.elemento == Elemento.agua &&
+          (jogador.elemento == Elemento.fogo || jogador.elemento == Elemento.terra)) ||
+      (inimigo.elemento == Elemento.terra &&
+          (jogador.elemento == Elemento.fogo || jogador.elemento == Elemento.ar)) ||
+      (inimigo.elemento == Elemento.ar && jogador.elemento == Elemento.fogo)) {
+    jogador.vida -= inimigo.ataque * 3;
+  } else {
+    jogador.vida -= inimigo.ataque;
+  }
+  return 0;
 }
 
 void main() {
@@ -68,15 +68,11 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(onPressed: () {setState(() {luta_pelo_inimigo();});}, icon: Icon(Icons.circle, color: Color.fromARGB(255, 255, 0, 0))),
-          title: IconButton(onPressed: () {setState(() {luta();});  }, icon: Icon(Icons.circle, color: Color.fromARGB(255, 0, 255, 0))),
-          actions: [IconButton(onPressed: () {setState(() {inimigo.vida += 100; jogador.vida += 100;}); }, icon: Icon(Icons.circle, color: Color.fromARGB(255, 255, 255, 0)))],
-        ),
         body: Center(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              /// INIMIGO
               Card(
                 elevation: 4,
                 margin: EdgeInsets.all(8),
@@ -91,21 +87,49 @@ class _MyAppState extends State<MyApp> {
                     ),
                     Padding(
                       padding: EdgeInsets.all(12),
-                      child: Column(children: [
-                        Text('Nome: Dragão Verde', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                        SizedBox(height: 8),
-                        Text(inimigo.vida.toString()),
-                        Text(inimigo.ataque.toString()),
-                      ]),
+                      child: Column(
+                        children: [
+                          Text('Nome: Dragão Verde', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                          SizedBox(height: 8),
+                          Text('Vida: ${inimigo.vida}'),
+                          Text('Ataque: ${inimigo.ataque}'),
+                          Text('Elemento: ${inimigo.elemento.name}'),
+                        ],
+                      ),
                     ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                luta_pelo_inimigo();
+                              });
+                            },
+                            child: Text("Atacar")),
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                inimigo.vida += 100;
+                              });
+                            },
+                            child: Text("Curar")),
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                inimigo.ataque = kleber.ataque;
+                                inimigo.vida = kleber.vida;
+                                inimigo.elemento = kleber.elemento;
+                              });
+                            },
+                            child: Text("Escolher")),
+                      ],
+                    )
                   ],
                 ),
               ),
-              ElevatedButton(onPressed: (){setState(() {
-                inimigo.ataque = kleber.ataque;
-                inimigo.vida = kleber.vida;
-                inimigo.elemento = kleber.elemento;
-              });}, child: Icon(Icons.abc_outlined)),
+
+              /// JOGADOR
               Card(
                 elevation: 4,
                 margin: EdgeInsets.all(8),
@@ -120,22 +144,47 @@ class _MyAppState extends State<MyApp> {
                     ),
                     Padding(
                       padding: EdgeInsets.all(12),
-                      child: Column(children: [
-                        Text('Nome: Fera Sombria', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                        SizedBox(height: 8),
-                        Text(jogador.vida.toString()),
-                        Text(jogador.ataque.toString()),
-                      ]),
+                      child: Column(
+                        children: [
+                          Text('Nome: Fera Sombria', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                          SizedBox(height: 8),
+                          Text('Vida: ${jogador.vida}'),
+                          Text('Ataque: ${jogador.ataque}'),
+                          Text('Elemento: ${jogador.elemento.name}'),
+                        ],
+                      ),
                     ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                luta();
+                              });
+                            },
+                            child: Text("Atacar")),
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                jogador.vida += 100;
+                              });
+                            },
+                            child: Text("Curar")),
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                jogador.ataque = jonnas.ataque;
+                                jogador.vida = jonnas.vida;
+                                jogador.elemento = jonnas.elemento;
+                              });
+                            },
+                            child: Text("Escolher")),
+                      ],
+                    )
                   ],
                 ),
-              ),ElevatedButton(onPressed: () {setState(() {
-                jogador.ataque = jonnas.ataque;
-                jogador.vida = jonnas.vida;
-                jogador.elemento = jonnas.elemento;});},
-  child: Icon(Icons.abc_outlined),
-),
-
+              ),
             ],
           ),
         ),
