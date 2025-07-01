b6f3a101f8cc17f0af7c179525a0b34d37eb4d4d
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-07-01 09:59:38 -0300

    FEAT: Add Various Elements in Attack Model

--- lib/core/models/attack_model.dart
+++ lib/core/models/attack_model.dart
@@ -4,9 +4,9 @@ import '../enums/elemento_enum.dart';
 class Attack {
   String name;
   int base_damage; 
-  Elemento elemento;
+  List<Elemento> elementos;
   
-  Attack(this.name, this.base_damage, this.elemento);
+  Attack(this.name, this.base_damage, this.elementos);
 
   // Cria um Map com os Multiplicadores de Dano de CADA ELEMENTO
   static final Map<Elemento, Map<Elemento, double>> _multiplicadores = {
@@ -48,13 +48,14 @@ class Attack {
   };
   
   double GetDamageMultiplier(Creature creature_) {
-    int elementosCount = creature_.elementos.length; // Pega o Número de Elementos na Criatura
     double total = 0.0; // Inicia a Variável com o Total da Multiplicação com 0
-
-    for (Elemento elemento_ in creature_.elementos) { // Para CADA Elemento na Criatura:
-      double mult = _multiplicadores[elemento]?[elemento_] ?? 1.0; // Se Existir na Tabela _multiplicadores, pegue o valor, se não, pegue o valor 1.0
-      total += mult * (1 / elementosCount); // Adiciona ao Total de acordo com a Fórmula no GDD  
-  }
+    for(Elemento elemento in elementos){ // Adiciona a possibilidade de um ataque ter VÁRIOS elementos
+      int elementosCount = creature_.elementos.length; // Pega o Número de Elementos na Criatura
+      for (Elemento elemento_ in creature_.elementos) { // Para CADA Elemento na Criatura:
+        double mult = _multiplicadores[elemento]?[elemento_] ?? 1.0; // Se Existir na Tabela _multiplicadores, pegue o valor, se não, pegue o valor 1.0
+        total += mult * (1 / elementosCount); // Adiciona ao Total de acordo com a Fórmula no GDD  
+      }
+    }
 
   return total;
 }
@@ -70,7 +71,7 @@ class Attack {
       return {
         'name': name,
         'base_damage': base_damage,
-        'elemento': elemento.index, // Salva o Enum como Inteiro
+        'elementos': elementos.map((e) => e.index).toList(), // Salva como Lista de Indexes
       };
     }
 
@@ -79,7 +80,7 @@ class Attack {
     return Attack(
       map['name'] as String,
       map['base_damage'] as int,
-      Elemento.values[map['elemento'] as int], // Converte Inteiro para Enum
+      (map['elementos'] as List).map((i) => Elemento.values[i]).toList(),
     );
   }
 }
