257d4bb409bae53eccea12f38a637b72e382d189
Jailson Stein <steinjailson@gmail.com>
2025-07-05 08:00:36 -0300

    FEAT: Classe Jogador

--- lib/core/models/jogador_model.dart
+++ lib/core/models/jogador_model.dart
@@ -1,1 +1,13 @@
-// Jailson fazendo
+class Jogador {
+  String nickName;
+  int cristais;
+  int level;
+  int amuletos;
+
+  Jogador({
+    required this.nickName,
+    required this.cristais,
+    required this.level,
+    required this.amuletos,
+  });
+}
