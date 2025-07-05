2750f88754896f798a74ac29318ced9ffd9967a5
Jailson Stein <steinjailson@gmail.com>
2025-07-05 08:13:56 -0300

    FEAT:  jogador_model.dart Completo

--- lib/core/models/jogador_model.dart
+++ lib/core/models/jogador_model.dart
@@ -1,13 +1,40 @@
 class Jogador {
+  int? id;
   String nickName;
   int cristais;
   int level;
   int amuletos;
 
   Jogador({
+    this.id,
     required this.nickName,
     required this.cristais,
     required this.level,
     required this.amuletos,
   });
+
+  // Convertendo para Map (para inserir no banco)
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      'nome': nickName,
+      'experiencia': cristais,
+      'nivel': level,
+      'amuletos': amuletos,
+    };
+    if (id != null) {
+      map['id'] = id;
+    }
+    return map;
+  }
+
+  // Convertendo de Map (vindo do banco)
+  factory Jogador.fromMap(Map<String, dynamic> map) {
+    return Jogador(
+      id: map['id'],
+      nickName: map['nome'],
+      cristais: map['experiencia'],
+      level: map['nivel'],
+      amuletos: map['amuletos'],
+    );
+  }
 }
