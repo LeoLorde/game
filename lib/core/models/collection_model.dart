f1cc30526290b44cc72b1566d02c968559ba8905
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-07-08 10:41:22 -0300

    REFACTOR: Creating Collection Model

--- lib/core/models/collection_model.dart
+++ lib/core/models/collection_model.dart
@@ -4,4 +4,20 @@ class Collection{
   List<Creature> creatures;
 
   Collection(this.id, this.creatures);
+
+   Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'creatures': creatures.map((c) => c.ToMap()).toList(),
+    };
+  }
+
+  factory Collection.fromMap(Map<String, dynamic> map) {
+    return Collection(
+      map['id'] as int,
+      (map['creatures'] as List<dynamic>)
+          .map((c) => Creature.fromMap(c as Map<String, dynamic>))
+          .toList(),
+    );
+  }
 }
