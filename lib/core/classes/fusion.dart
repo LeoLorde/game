069a9e093867ca450be08c84135eaeeabf766c43
dallalaste <gabrieldallelaste75@gmail.com>
2025-07-17 11:51:38 -0300

    feat: adicionar a estrutura de fusão mesmo

--- lib/core/classes/fusion.dart
+++ lib/core/classes/fusion.dart
@@ -2,5 +2,17 @@ import '../../core/models/creature_model.dart';
 import 'package:game/database/dao/creature_dao.dart';
 
 class Fusion{
-  
+//Receber dados do personagem (nível e elemento)
+
+//Fazer a média de nível considerando a raridade
+
+//Fazer a verificação de raridade (se subiu de raridade aleatoriamente)
+
+//Caso a raridade for aumentada o nível médio cai pela metade (nível médio * 0,5)
+
+//Armazenar os elementos das 2 cartas selecionadas dentro de uma lista
+
+//Fazer a verificação se essa lista de elementos criados se encontra em uma carta, senão, seleciona a mais próxima
+
+//Cria a nova carta gerada baseada nos elementos e no nível já recebidos acima
 }
