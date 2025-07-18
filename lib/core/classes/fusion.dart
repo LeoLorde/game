26335e296f11a6095be35db24a3cedd1e33f0038
dallalaste <gabrieldallelaste75@gmail.com>
2025-07-18 09:36:54 -0300

    feat: verificar dados das cartas fusionadas

--- lib/core/classes/fusion.dart
+++ lib/core/classes/fusion.dart
@@ -1,8 +1,11 @@
-import 'package:game/core/enums/elemento_enum.dart';
+import 'dart:math';
 
+import 'package:game/core/enums/elemento_enum.dart';
 import '../../core/models/creature_model.dart';
 import 'package:game/database/dao/creature_dao.dart';
 import 'package:game/database/seed/creature_seed.dart';
+import 'package:game/core/enums/raridade_enum.dart';
+import 'dart:math';
 
 class Fusion {
   Creature criatura1 = CreatureSeed.all_creatures[0];//emula uma criatura que o jogador vai fundir
@@ -10,6 +13,9 @@ class Fusion {
 
   int get nv_criatura1 => criatura1.level;//pega o nível da primeira criatura
   int get nv_criatura2 => criatura2.level;//pega o nível da segunda criatura
+  int get nivelMedio => ((nv_criatura1 + nv_criatura2) / 2).round();//faz a media de níveis
+  Raridade get raridade_criatura1 => criatura1.raridade;
+  Raridade get raridade_criatura2 => criatura2.raridade;
 
   List<Elemento> get ele_criatura1 => criatura1.elementos;//pega os elementos da primeira criatura
   List<Elemento> get ele_criatura2 => criatura2.elementos;//pega os elementos da segunda criatura
@@ -17,10 +23,24 @@ class Fusion {
   List<Elemento> get elementosFundidos {//junta os elementos da primeira e da segunda criatura em uma lista só
     return {...ele_criatura1, ...ele_criatura2}.toList();//transforma em lista mesmo
   }
+    
+    //Fazer a verificação de raridade (se subiu de raridade aleatoriamente)
+    final Random _random = Random();
+
+  /// Calcula a chance com base em um valor entre 0 e 1000
+  /// Cada ponto representa 0.1% de chance
+  double calcularChance(int valor) {
+    return (valor * 0.1).clamp(0.0, 100.0); // garante entre 0% e 100%
+  }
+
+  /// Retorna true se o evento acontecer com base na chance
+  bool eventoAcontece(int valor) {
+    double chance = calcularChance(valor);
+    double sorteio = _random.nextDouble() * 100; // número entre 0.0 e 100.0
+    return sorteio <= chance;
+  }
 
     //Fazer a média de nível considerando a raridade
-
-    //Fazer a verificação de raridade (se subiu de raridade aleatoriamente)
 
     //Caso a raridade for aumentada o nível médio cai pela metade (nível médio * 0,5)
 
