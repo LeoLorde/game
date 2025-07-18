1a1333f9753242d0ed98aeaa06fe24f8b1cb0331
Jailson Stein <steinjailson@gmail.com>
2025-07-18 09:39:03 -0300

    CHORE: Battle.dart

---
+++ lib/core/classes/battle.dart
@@ -0,0 +1,63 @@
+class Battle {
+  final String nameJogador;
+  final String nameBot;
+  final String description;
+  final String imageUrl;
+  final String backgroundImageUrl;
+  final List<int> playerDeckIds;
+  final List<int> botDeckIds;
+
+  Battle({
+    required this.nameJogador,
+    required this.nameBot,
+    required this.description,
+    required this.imageUrl,
+    required this.backgroundImageUrl,
+    required this.playerDeckIds,
+    required this.botDeckIds,
+  });
+}
+
+// void deckJogador{
+//   Implementação do deck do jogador
+// }
+
+// void deckBot{
+//   Implementação do deck do bot
+// }
+
+// void ataque{
+//   Implementação do ataque
+// }
+
+// void vitoria{
+//   Implementação da vitória
+// }
+
+// void calcularDano{
+//   Implementação do cálculo de dano
+// }
+
+// void calcularVida{
+//   Implementação do cálculo de vida
+// }
+
+// void alterarCriatura{
+//   Implementação da alteração de criatura
+// }
+
+// void talismanesJogador{
+//   Implementação dos talismanes do jogador
+// }
+
+// void talismanesBot{
+//   Implementação dos talismanes do bot
+// }
+
+// void usarTalismanes{
+//   Implementação do uso de talismanes
+// }
+
+// void calcularEfeitoBonus{
+//   Implementação do cálculo de efeito bônus com base na raridade(seguindo o GDD)
+// }
